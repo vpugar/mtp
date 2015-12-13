1eb466649c859ffa9d264eabc890b8d43386570f
vpugar <vpugar@gmail.com>
2015-12-13 14:56:33 +0100

    transaction validator logging

--- mtp-processor/src/main/java/com/vedri/mtp/processor/transaction/TransactionValidator.java
+++ mtp-processor/src/main/java/com/vedri/mtp/processor/transaction/TransactionValidator.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.Set;
 
 import com.vedri.mtp.core.MtpConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import com.vedri.mtp.core.transaction.Transaction;
 import com.vedri.mtp.core.transaction.aggregation.TransactionValidationStatus;
 
 @Component
+@Slf4j
 public class TransactionValidator {
 
 	private final CountryManager countryManager;
@@ -25,6 +27,7 @@ public class TransactionValidator {
 	public TransactionValidationStatus validate(Transaction transaction) {
 
 		if (!checkNegativeAmount(transaction)) {
+			log.warn("Invalid amount for {}", transaction.getTransactionId());
 			return TransactionValidationStatus.InvalidAmount;
 		}
 		if (!checkAmountSanity(transaction)) {
@@ -41,8 +44,13 @@ public class TransactionValidator {
 
 	private boolean checkAmountSanity(Transaction transaction) {
 		final BigDecimal amountBuy2 = transaction.getAmountSell().multiply(transaction.getRate());
-		return amountBuy2.setScale(MtpConstants.CURRENCY_SCALE, MtpConstants.CURRENCY_ROUNDING)
-				.equals(transaction.getAmountBuy());
+		final BigDecimal scaledValue = amountBuy2.setScale(MtpConstants.CURRENCY_SCALE, MtpConstants.CURRENCY_ROUNDING);
+		final boolean equals = scaledValue.equals(transaction.getAmountBuy());
+		if (!equals) {
+			log.warn("Incorrect rate for {} - amountBuy: {}, calculatedValue: {}",
+					transaction.getTransactionId(), transaction.getAmountBuy(), scaledValue);
+		}
+		return equals;
 	}
 
 	private TransactionValidationStatus checkOriginatingCountry(Transaction transaction) {
@@ -50,12 +58,14 @@ public class TransactionValidator {
 		final Country country = countryManager.getCountryFromCca2(originatingCountry);
 
 		if (country == null) {
+			log.warn("Invalid country for {}", transaction.getTransactionId());
 			return TransactionValidationStatus.InvalidCountry;
 		}
 		else {
 			final Set<Country> fromCurrency = countryManager.getCountriesFromCurrency(transaction.getCurrencyFrom());
 
 			if (fromCurrency.size() == 0) {
+				log.warn("Invalid from currency for {}", transaction.getTransactionId());
 				return TransactionValidationStatus.InvalidFromCurrency;
 			}
 			// TODO check if this is valid check
@@ -66,6 +76,7 @@ public class TransactionValidator {
 			final Set<Country> toCurrency = countryManager.getCountriesFromCurrency(transaction.getCurrencyTo());
 
 			if (toCurrency.size() == 0) {
+				log.warn("Invalid to currency for {}", transaction.getTransactionId());
 				return TransactionValidationStatus.InvalidToCurrency;
 			}
 		}
